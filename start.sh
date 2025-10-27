e751d3303be89421f7f25cf5dc234d289aede172
4scee <ascebar@gmail.com>
2025-10-26 21:55:09 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Activar entorno virtual si existe
-if [ -d ".venv" ]; then
-    source .venv/bin/activate
-fi
+# Salir si hay algún error
+set -e
 
-# Instalar dependencias
-pip install -r requirements.txt
+# Instalar dependencias usando pip3
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
-# Ejecutar la app Flask en el puerto de Railway
-python app.py
+# Ejecutar la app Flask en el puerto asignado por Railway
+python3 app.py
